b43b8947ff71739187096afab665004d97eab331
hammadAuthornate <156046487+hammadAuthornate@users.noreply.github.com>
2025-04-04 17:13:36 +0500

    updated jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,9 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testTimeout: 20000
+    testTimeout: 20000,
+    testPathIgnorePatterns: [
+      "/node_modules/",
+      "\\.test\\.d\\.ts$"  // Ignore files ending with .test.d.ts
+    ]
   };
